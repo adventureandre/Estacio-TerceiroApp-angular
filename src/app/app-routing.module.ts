5f4fcf04075f936f0235ec56dcd154226a3a0b90
Andre Luiz <adventureandre@hotmail.com>
2023-04-29 17:51:08 -0300

    terceiro app angular estacio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NovProdComponent } from './nov-prod/nov-prod.component';
+import { ProdutoComponent } from './produto/produto.component';
+import {LoginComponent} from "./login/login.component";
+import {LoginService} from "./login.service";
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'produto', component: ProdutoComponent,
+    canActivate: [LoginService] },
+  { path: 'novoprod', component: NovProdComponent,
+    canActivate: [LoginService] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NovProdComponent } from './nov-prod/nov-prod.component';
+import {ProdutoService} from "./produto.service";
+import {FormsModule} from "@angular/forms";
+import {LoginService} from "./login.service";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    LateralComponent,
+    HomeComponent,
+    ProdutoComponent,
+    NovProdComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProdutoService,LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
